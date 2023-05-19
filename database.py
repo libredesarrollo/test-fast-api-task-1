f7242bc1319f07394e36daa31b6f1799f6af6f10
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2023-05-19 08:28:44 -0400

    varios

--- database.py
+++ database.py
@@ -14,7 +14,8 @@ Base = declarative_base()
 
 
 # DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/fastapi_task"
-DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/fastapi_task"
+# DATABASE_URL = "mysql+mysqlconnector://root@localhost:3306/fastapi_task"
+DATABASE_URL = "mysql+mysqlconnector://sail:password@localhost:3306/testing"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
